0470dcabecb7a392ca7b17e050a7aace6d036c0a
Anis Kehila <aniskehila22@gmail.com>
2023-01-06 22:26:54 +0100

    made the contact form responsive

--- main.js
+++ main.js
@@ -6,9 +6,8 @@ window.addEventListener("scroll",() => {
         header.classList.remove('scrolled');
     }
 });
-
+// boxe swiper in phone && Tablets 
 let init = false;
-
 function swiperCard() {
     if (window.innerWidth <= 910) {
         if (!init) {
@@ -18,7 +17,8 @@ function swiperCard() {
                 slidesPerView: "auto",
                 centeredSlides: true,
                 spaceBetween: 56,
-        });
+                loop: true
+            });
         }
     } else if (init) {
         swiper.destroy();
@@ -27,3 +27,49 @@ function swiperCard() {
 }
 swiperCard();
 window.addEventListener("resize", swiperCard);
+
+// offers swiper in phone 
+let isInit = false;
+function swiperOffers() {
+    if (window.innerWidth <= 768) {
+        if (!isInit) {
+            isInit = true;
+            swiper = new Swiper(".mySecSwiper", {
+                direction: "horizontal",
+                slidesPerView: "auto",
+                centeredSlides: true,
+                spaceBetween: 32,
+                loop: true
+            });
+        }
+    } else if (isInit) {
+        swiper.destroy();
+        isInit = false;
+    }
+}
+swiperOffers();
+window.addEventListener("resize", swiperOffers);
+// Click for change content in offers
+let offerBtns = document.querySelectorAll('.offers .swiper-slide');
+let content = document.querySelectorAll('.content');
+
+function offerBtnsHandler() {
+    offerBtns.forEach((btn) => btn.addEventListener('click' , () => {
+        offerBtns.forEach(rmvClass => rmvClass.classList.remove('swiper-slide-active'));
+        btn.classList.toggle('swiper-slide-active');
+    })
+    );
+}
+if (window.innerWidth >= 768) {
+    offerBtnsHandler();
+}
+content.forEach((contEle) => {
+    offerBtns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            if(contEle.getAttribute('data-content') === btn.getAttribute('data-content') && btn.classList.contains('swiper-slide-active')) {
+                content.forEach((removeEle) => removeEle.classList.remove('active'));
+                contEle.classList.add('active');
+            }
+        })
+    })
+});
